23f2045e11c2ae51b73b553b1049e68a6a265bf7
Max Nerius <max.nerius@proton.me>
2024-10-22 14:02:46 +0200

    Add: Tests for `Pager` and `CapCode`

---
+++ webservice/tests/Entity/CapCodeTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Tests\Entity;
+
+use App\Entity\CapCode;
+use PHPUnit\Framework\TestCase;
+
+final class CapCodeTest extends TestCase
+{
+    public function testCreateValidCapCode(): void
+    {
+        $cc = new CapCode(3);
+        $this->assertTrue(3 === $cc->getCode());
+    }
+
+    public function testCreateInvalidCapCode(): void
+    {
+        foreach ([-1, 0, 9999999, 10000000] as $testValue) {
+            $this->expectException(\InvalidArgumentException::class);
+            new CapCode($testValue);
+        }
+    }
+}
